1c440790e38fb39de15a55fec0d9f86d805e936a
steven <steven_wu@sanwa-eng.com.tw>
2018-07-20 16:06:24 +0800

    no need

--- WindowsFormsApp1/sql/DIO Setting Log.sql
+++
@@ -1,7 +0,0 @@
-﻿SELECT t.function_type, t.action_type, t.remark, t.action_user,
-       date_format(action_time,'%Y-%m-%d %H:%i:%s.%f') AS action_time
-  FROM log_system_action t
- WHERE action_time BETWEEN @from_dt AND @to_dt
-   AND action_user LIKE @cond_1
-   AND t.action_type = 'FormDIOSetting'
- ORDER BY action_time;
